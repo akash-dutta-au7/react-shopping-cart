a1879faf0527b60c6f6e274508f4204db56dbeae
Akash Dutta <dutta.akash1993official@gmail.com>
2020-10-09 22:33:21 +0530

    product page done

---
+++ src/components/ProductItem/index.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import "./index.css";
+const ProductItem = ({ product }) => {
+  console.log(product.title);
+  return (
+    <Fragment>
+      <li key={product._id} className="product-li">
+        <div className="product-item">
+          <div className="item-container">
+            <a href={"#" + product._id}>
+              <img src={product.image} alt={product.title} />
+            </a>
+            <p className="product-title">{product.title}</p>
+            <div className="price-area">
+              <p>INR {product.price}</p>
+              <button className="add-to-cart-btn" type="submit">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </li>
+    </Fragment>
+  );
+};
+
+export default ProductItem;
