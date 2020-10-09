c4348e20b70f9f5d084551f1ba42c54f71d40ded
Akash Dutta <dutta.akash1993official@gmail.com>
2020-10-10 01:08:37 +0530

    filter and sort done

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import data from "../../data.json";
+import Filter from "../Filter";
 import ProductItem from "../ProductItem";
 import "./index.css";
 const Products = () => {
@@ -8,11 +9,54 @@ const Products = () => {
     size: "",
     sort: "",
   });
+
+  const sortByPrice = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    if (value === "") {
+      setAllItems({
+        products: data.products,
+      });
+    } else if (value === "low-tier") {
+      setAllItems({
+        products: data.products.filter((product) => product.price <= 399),
+      });
+    } else if (value === "mid-tier") {
+      setAllItems({
+        products: data.products.filter(
+          (product) => product.price >= 400 && product.price <= 699
+        ),
+      });
+    } else {
+      setAllItems({
+        products: data.products.filter((product) => product.price >= 700),
+      });
+    }
+  };
+
+  const filterBySize = (e) => {
+    if (e.target.value === "") {
+      setAllItems({ size: e.target.value, products: data.products });
+    }
+    setAllItems({
+      size: e.target.value,
+      products: data.products.filter(
+        (product) => product.availableSizes.indexOf(e.target.value) >= 0
+      ),
+    });
+  };
   return (
     <div className="container">
+      <Filter
+        product={allItems.products.length}
+        size={allItems.size}
+        sort={allItems.sort}
+        sortByPrice={sortByPrice}
+        filterBySize={filterBySize}
+      />
       <div className="all-products">
         {allItems.products.map((product) => (
-          <ProductItem product={product} />
+          <ProductItem product={product} key={product._id} />
         ))}
       </div>
     </div>
